82547dcd188151a942211e5251d7ee820cbe7b54
Everton Mendonca Jesus <evertonmj@gmail.com>
2026-04-24 14:44:20 -0300

    feat: add GitHub Actions workflow for deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/interligados-v2/', // Caminho do repositório para GitHub Pages
   plugins: [
     react(),
     tailwindcss(),
